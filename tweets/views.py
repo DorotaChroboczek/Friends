65fc2d22daa5d04472ebb3b4f03b7f74a05d8366
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-04-25 20:56:49 +0200

    feature: implement tweet detail view - dynamic routing

--- tweets/views.py
+++ tweets/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
