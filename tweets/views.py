76757137e9b8e740da5be81f13f11e06c37624d6
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-05-01 20:45:46 +0200

    feature: implement tweet_list_view

--- tweets/views.py
+++ tweets/views.py
@@ -8,6 +8,20 @@ def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html', context={}, status=200)
 
 
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JS or Swift/Java/iOS/Android
+    return Json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{'id': x.id, 'content': x.content} for x in qs]
+    data = {
+        'response': tweets_list
+    }
+    return JsonResponse(data)
+
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
