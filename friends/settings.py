19d1e1617fd620f8a7424b6572f2cedca3813dc9
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-05-02 15:38:57 +0200

    feature: Safe URL Redirect

--- friends/settings.py
+++ friends/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+$lh-4cx4z%$s#z*aoe4=@=*!@wbj+)5$p4ko479l03vhzr^vn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
